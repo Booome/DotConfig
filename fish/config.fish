c0b877198d22c4a7bf68f221c624a54067721b78
Booome <604772159@qq.com>
2020-05-10 09:56:44 +0800

    add git_switch function

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,18 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 function fish_greeting
     fortune -c | cowsay
 end
+
+function git_switch_to_booome
+    set -l __url git@github.booome:(git remote get-url origin | cut -d: -f2)
+    git remote set-url origin $__url
+    git config user.name "Booome"
+    git config user.email "604772159@qq.com"
+end
+
+function git_switch_to_work
+    set -l __url git@github.com:(git remote get-url origin | cut -d: -f2)
+    git remote set-url origin $__url
+    git config user.name "BodongLiKolmostar"
+    git config user.email "bodong.li@kolmostar.com"
+end
+
