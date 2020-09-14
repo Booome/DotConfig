71d3ffc5144ecf13b383006ccbc33274661adf24
BodongLiKolmostar <bodong.li@kolmostar.com>
2020-09-14 16:22:59 +0800

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,8 @@ set -x TERM linux
 alias vim='nvim'
 set -x EDITOR nvim
 
-set -x http_proxy http://127.0.0.1:8123
-set -x https_proxy http://127.0.0.1:8123
+set -x http_proxy http://127.0.0.1:8888
+set -x https_proxy http://127.0.0.1:8888
 
 set -U FZF_LEGACY_KEYBINDINGS 0
 
